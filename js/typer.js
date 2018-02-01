5125fc263472f691fa38939f3885e594b9a63664
MoonlightOwl <murky.owl@gmail.com>
2018-02-01 23:13:40 +0200

    let typing emulator make errors sometimes and then correct them

--- js/typer.js
+++ js/typer.js
@@ -2,11 +2,16 @@
  * An ever programming version of myself :3
  */
 
+var TYPING = 0, MISTYPING = 1, BACKSPACING = 2;
+
 var MAX_LINES = 5;               // how long will be the visible part of a listing
 var lines = 0;                   // how long it is right now
 var listing = ". . . . . .";     // the full listing itself
 var printed = "";                // the visible part of it
 var position = 0;                // cursor position
+var ERRORS_RATE = 0.1;           // the probability of typing wrong word
+var error_position = -1;         // some random word from the listing, printed 'by error'
+var state = TYPING;              // do 'I' write the code or undo some errors
 var language = "lua";            // current language (for syntax highlighting)
 
 // emulation container
@@ -19,22 +24,66 @@ function rewind() {
     else printed = printed.substring(printed.indexOf("\n") + 1);
 }
 
+function isLetter(char) {
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
+// search for random word in the listing (for 'errors' emulation)
+function findErrorPosition() {
+    var position = Math.round(Math.random() * listing.length);
+    // adjust to the beginning of the next word
+    while (position < listing.length && isLetter(listing[position])) position++;
+    while (position < listing.length && !isLetter(listing[position])) position++;
+    //
+    if (position < listing.length)
+        return position;
+    else
+        return -1;
+}
+
+function ongoingChar() {
+    return listing[ state === MISTYPING ? error_position : position ]
+}
+function atBeginningOfWord() {
+    return isLetter(ongoingChar()) && !isLetter(printed.slice(-1));
+}
+function atEndOfWord() {
+    return !isLetter(ongoingChar()) && isLetter(printed.slice(-1));
+}
+
 // do yet another letter
 function type() {
-    printed += listing[position];
-    if (listing[position] === '\n') rewind();
-    scroller.innerHTML = hljs.highlight(language, printed, true).value;
-
-    if (position < listing.length - 1) {
-        position ++;
-        var timeout = Math.random() * ((position < listing.length && listing[position] !== ' ' && listing[position + 1] === ' ') ? 450 : 60);
-        setTimeout(type, timeout);
+    // what am I thinking of?
+    if (state === TYPING && atBeginningOfWord() && Math.random() < ERRORS_RATE) {
+        error_position = findErrorPosition();
+        if (error_position !== -1) state = MISTYPING;
+    } else if (state === MISTYPING && atEndOfWord()) {
+        state = BACKSPACING;
+    } else if (state === BACKSPACING && atBeginningOfWord()) {
+        state = TYPING;
     }
+    // type
+    if (state === BACKSPACING)
+        printed = printed.slice(0, -1);
     else {
+        printed += ongoingChar();
+        if (ongoingChar() === '\n') rewind();
+    }
+    // update HTML
+    scroller.innerHTML = hljs.highlight(language, printed, true).value;
+    // move pointer
+    if (state === TYPING) position++;
+    if (state === MISTYPING) error_position++;
+    if (position >= listing.length) {
         rewind();
         printed += "\n";
         position = 0;
         fetch();
+    } else if (error_position >= listing.length) {
+        state = BACKSPACING;
+    } else {
+        var timeout = Math.random() * (state === BACKSPACING ? 180 : (atEndOfWord() ? 450 : 60));
+        setTimeout(type, timeout);
     }
 }
 
